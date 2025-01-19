d16bb9d774a1c4b6eba696a94062fcb40687a71a
tasniaarhamarham <tasniaarhamarham@gmail.com>
2025-01-20 01:22:45 +0600

    someting learning

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,19 +21,27 @@ window.addEventListener("scroll", scrollHeader);
 
 const sections = document.querySelectorAll('section[id]');
 
-const scrollActive = () => {
-  const scrollDown = window.scrollY;
+// const scrollActive = () => {
+//   const scrollDown = window.scrollY;
 
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight,
-      sectionTop = current.offsetTop - 58,
-      sectionId = current.getAttribute('id'),
-      sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+//   sections.forEach((current) => {
+//     const sectionHeight = current.offsetHeight,
+//       sectionTop = current.offsetTop - 58,
+//       sectionId = current.getAttribute('id'),
+//       sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
 
-    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
-      sectionsClass.classList.add('active-link');
-    } else {
-      sectionsClass.classList.remove('active-link');
-    }
-  });
+//     if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+//       sectionsClass.classList.add('active-link');
+//     } else {
+//       sectionsClass.classList.remove('active-link');
+//     }
+//   });
+// };
+// window.addEventListener('scroll', scrollActive);
+
+const scrollup = () => {
+  const scrollUp = document.getElementById('scroll-up');
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
 };
+
+window.addEventListener('scroll', scrollup);
