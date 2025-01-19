1ec6c6f3a827e6285b60d6589f0492abc6340768
tasniaarhamarham <tasniaarhamarham@gmail.com>
2025-01-19 16:42:17 +0600

    hero section ready without animated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// menu button & close button
 const navClose = document.getElementById("close-btn"), navToggle = document.getElementById("nav-toggle"), navMenu = document.getElementById("nav-menu");
 navClose.addEventListener("click", () => {
     navMenu.classList.remove("show-menu");
@@ -8,3 +9,31 @@ navToggle.addEventListener("click", () => {
     navMenu.classList.add("show-menu");
     navToggle.classList.add("active");
 });
+
+//=============== Shadow HEADER ===============
+const scrollHeader=()=>{
+    const header = document.getElementById("header");
+    this.scrollY >= 50 ? header.classList.add("scroll-header") : header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+
+
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id'),
+      sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link');
+    } else {
+      sectionsClass.classList.remove('active-link');
+    }
+  });
+};
